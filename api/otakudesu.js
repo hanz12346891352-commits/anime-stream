368d4e28882c345b431f4d6cd3744b364b7f0e54
hanz12346891352-commits <hanz12346891352@gmail.com>
2026-07-03 01:32:29 +0700

    Create otakudesu.js

---
+++ api/otakudesu.js
@@ -0,0 +1,13 @@
+export default async function handler(request) {
+  const url = new URL(request.url);
+  const path = url.pathname.replace('/api/otakudesu', ''); 
+  const targetUrl = `https://otakudesu.blog${path}${url.search}`; // FIXED
+
+  const res = await fetch(targetUrl, {
+    headers: { 'User-Agent': 'Mozilla/5.0', 'Referer': 'https://otakudesu.blog/' }
+  });
+  return new Response(await res.text(), {
+    status: res.status,
+    headers: { 'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*', 'Cache-Control': 's-maxage=300' }
+  });
+}
